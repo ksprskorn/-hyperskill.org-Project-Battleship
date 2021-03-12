b391f24fbd56b1a9c9f772aa61cc67ec787a4e63
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2021-03-12 15:55:07 +0200

    commit Problems (my solutions)

--- Problems/Getting submaps/src/Main.java
+++ Problems/Getting submaps/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer fromRange = scanner.nextInt();
+        Integer toRange = scanner.nextInt() + 1;
+        int lineCount = scanner.nextInt();
+
+        SortedMap<Integer, String> sm = new TreeMap<>();
+        int i = 0;
+        while (i < lineCount) {
+            Integer number = scanner.nextInt();
+            String str = scanner.next();
+            sm.put(number,str);
+            i++;
+        }
+
+        sm.subMap(fromRange,toRange).forEach((key,value) -> System.out.println(key + " " + value));
 
     }
 }
